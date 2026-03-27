0d560ecd67af5272855466070888903fe114bcb9
TacoConKvass <e_frun@o2.pl>
2026-03-27 22:51:38 +0100

    Update: Ehance `OnHit<T>` functionality

--- src/Terraria_JJK/Common/Components/OnHit.cs
+++ src/Terraria_JJK/Common/Components/OnHit.cs
@@ -4,13 +4,12 @@ public enum TargetType : byte
 {
 	Self,
 	Victim,
-	None,
 }
 
 [EC.Component(Wraps = [
-	typeof(Fade), typeof(ApplyBuff), typeof(Shoots)
+	typeof(Fade), typeof(ApplyBuff), typeof(Shoots), typeof(Trail)
 ])]
-public record struct OnHit<T>(T Inner) where T : struct
+public record struct OnHit<T>(T Inner, TargetType Target = TargetType.Victim) where T : struct
 {
 	static OnHit() {
 		DaybreakHooks.GlobalProjectileHooks.OnHitNPC.Event += OnProjectileHitNPC;
@@ -20,11 +19,30 @@ public record struct OnHit<T>(T Inner) where T : struct
 		orig(projectile, target, hit, damageDone);
 
 		if (!projectile.TryGet<OnHit<T>>(out var data)) return;
-		TryTrigger(projectile, target, data.Inner);
+		TryTrigger(projectile, target, data);
 	}
 
-	static void TryTrigger(Terraria.Entity source, Terraria.Entity entity, T data) {
-		if (data is not Core.ITriggerable i) return;
-		i.Trigger(source, entity);
+	static void TryTrigger(Terraria.Entity source, Terraria.Entity entity, OnHit<T> data) {
+		if (data.Inner is Core.ITriggerable i) {
+			i.Trigger(source, entity);
+			return;
+		}
+
+		if (data.Target == TargetType.Victim) {
+			_ = entity switch {
+				Terraria.NPC npc => npc.With(data.Inner),
+				Terraria.Player player => player.With(data.Inner),
+				_ => data.Inner,
+			};
+			return;
+		}
+
+		_ = source switch {
+			Terraria.NPC npc => npc.With(data.Inner),
+			Terraria.Item item => item.With(data.Inner),
+			Terraria.Player player => player.With(data.Inner),
+			Terraria.Projectile projectile => projectile.With(data.Inner),
+			_ => data.Inner,
+		};
 	}
 }
