8077796ad5146bedc0ef6c7c18f7bb4f6877d788
TacoConKvass <e_frun@o2.pl>
2026-04-29 19:39:25 +0200

    feat(components): resetting `OnTimer<T>` to allow for looping it

--- src/Terraria_JJK/Common/Components/OnTimer.cs
+++ src/Terraria_JJK/Common/Components/OnTimer.cs
@@ -9,19 +9,27 @@ public struct OnTimer<T> where T : struct, ITimeable
 		DaybreakHooks.GlobalProjectileHooks.AI.Event += TickProjectile;
 	}
 
+	public bool Resettable;
 	public T Inner;
 	public int Timer;
+	private int? orig_timer;
 
 	internal static void TickProjectile(DaybreakHooks.GlobalProjectileHooks.AI.Original orig, TML.GlobalProjectile self, Terraria.Projectile projectile) {
 		orig(projectile);
 		if (!projectile.TryGet<OnTimer<T>>(out var data)) return;
 
 		if (data.Timer > 0) {
-			projectile.Set(data with { Timer = data.Timer - 1 });
+			projectile.Set(data with {
+				Timer = data.Timer - 1,
+				orig_timer = data.orig_timer ?? data.Timer
+			});
 			return;
 		}
 
-		projectile.Disable<OnTimer<T>>();
+		if (!data.Resettable) projectile.Disable<OnTimer<T>>();
+		else {
+			projectile.Set(data with { Timer = data.orig_timer!.Value });
+		}
 		projectile.With(data.Inner);
 	}
 }
