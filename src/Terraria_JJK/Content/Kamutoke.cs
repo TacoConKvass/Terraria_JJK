1e962972992e2c1ae761c24d75442d3cfeaff1b7
TacoConKvass <e_frun@o2.pl>
2026-04-30 23:45:54 +0200

    feat(content): improve Kamutoke projectile behaviour

--- src/Terraria_JJK/Content/Kamutoke.cs
+++ src/Terraria_JJK/Content/Kamutoke.cs
@@ -30,25 +30,21 @@ public class KamutokeLightning : TML.ModProjectile
 	public static int ID => TML.ModContent.ProjectileType<KamutokeLightning>();
 
 	public override void SetDefaults() {
-		Projectile.Size = new FNA.Vector2 { X = 20, Y = 20 };
+		Projectile.Size = new FNA.Vector2 { X = 16, Y = 16 };
 		Projectile.friendly = true;
 		Projectile.extraUpdates = 20;
-		Projectile.timeLeft = 25 * Projectile.extraUpdates;
+		Projectile.timeLeft = 30 * Projectile.extraUpdates;
+		Projectile.usesLocalNPCImmunity = true;
+		Projectile.localNPCHitCooldown = -1;
 
-		Projectile.With(new Components.RotateWithVelocity { });
-		Projectile.With(new Components.OnTimer<Components.SpawnDust> {
-			Timer = 3,
-			Resettable = true,
-			Inner = new Components.SpawnDust {
-				Type = Terraria.ID.DustID.WitherLightning,
-				RelativePosition = () => FNA.Vector2.Zero,
-				Velocity = () => FNA.Vector2.Zero,
-				Count = 1,
-				Alpha = 128,
-				AffectedByGravity = false,
-				Perfect = true,
-			}
+		Projectile.With(new Components.Trail {
+			MaxPositions = Projectile.timeLeft,
+			Width = static (progress) => progress > 0 && progress < 1 ? 2 : 0,
+			Color = static (_) => FNA.Color.Aqua,
+			Delay = Projectile.extraUpdates * 2,
+			FadeDelay = 1,
 		});
+		Projectile.With(new Components.Trail.AddPosition { }); // Force first position to be added
 		Projectile.With(new Components.OnTimer<Components.SpawnProjectile> {
 			Timer = 30,
 			Inner = new Components.SpawnProjectile {
@@ -70,12 +66,18 @@ public class KamutokeLightning : TML.ModProjectile
 	FNA.Vector2 ChildVelocity() => Projectile.velocity.RotatedBy(10 * (1 + Projectile.ai[1]) * FNA.MathHelper.TwoPi / 360f);
 
 	static float[] angles = [-1f, 0.5f, 1f];
+
 	FNA.Vector2 ChangeVelocity(FNA.Vector2 old) {
+		foreach (var npc in Terraria.Main.ActiveNPCs) {
+			if (Projectile.Center.DistanceSQ(npc.Center) < 64 * 64)
+				return Projectile.DirectionTo(npc.Center);
+		}
+
 		if (Terraria.Main.netMode != Terraria.ID.NetmodeID.MultiplayerClient)
 			Projectile.ai[2] = angles[System.Math.Clamp((int)Projectile.ai[2] * -1, -1, 1) + 1] * Terraria.Main.rand.NextFloat(30, 45);
+
 		return old.RotatedBy(Projectile.ai[2] * FNA.MathHelper.TwoPi / 360f);
 	}
-
 
 	void ChildCallback(Terraria.Projectile child) {
 		if (Projectile.ai[1] > -1) {
@@ -91,12 +93,11 @@ public class KamutokeLightning : TML.ModProjectile
 					},
 				});
 		}
-		if (Projectile.ai[0] > 1) {
+		if (Projectile.ai[0] >= 1) {
 			child.Disable<Components.OnTimer<Components.SpawnProjectile>>();
 			return;
 		}
-		Projectile.ai[0]++;
-		child.ai[0] = Projectile.ai[0];
+		child.ai[0] = Projectile.ai[0] + 1;
 		_ = child.TryGet(out Components.OnTimer<Components.SpawnProjectile> data);
 		child.Set(data with {
 			Timer = (int)(data.Timer * Terraria.Main.rand.NextFloat(0.75f, 3f))
